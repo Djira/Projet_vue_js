3b9aff69e738724f0af36a849c926c9f4afeaf93
Djira <djcoully7@gmail.com>
2023-05-13 01:19:43 +0000

    third commit

--- src/views/IssueList.vue
+++ src/views/IssueList.vue
@@ -1,65 +1,53 @@
 <template>
-    <div>
-      <h1>issueリスト</h1>
-      <el-button type="success" @click="getIssues()">issue取得</el-button>
-      <el-row :gutter="12">
-        <el-col :span="12" v-for="issue in issues" :key="issue.id">
-          <el-card class="box-card" shadow="hover" style="margin: 5px 0;">
-            <el-row :gutter="12">
-              <el-col :span="21">{{ issue.title }}</el-col>
-              <el-col :span="3">
-                <!-- コード1 -->
-                <el-button @click="closeIssue(issue.number)" type="success" icon="el-icon-check" circle></el-button>
-              </el-col>
-            </el-row>
-          </el-card>
-        </el-col>
-      </el-row>
-    </div>
-  </template>
-    
-    <script>
-    import axios from 'axios';
-    
-    const client = axios.create({
-     baseURL: `${process.env.VUE_APP_GITHUB_ENDPOINT}`, 
-      headers: {
-        'Accept': 'application/vnd.github.v3+json',
-        'Content-Type':'application/json',
-        // Add the following
-       'Authorization': `token ${process.env.VUE_APP_GITHUB_TOKEN}`
-         //'Authorization': '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'
-      },
-    })
-    
-    export default {
-      name: ' IssueList',
-      data() {
-        return {
-          issues: []
-        }
-      },
-      methods: {
-      getIssues() {
-        client.get('/issues')
-          .then((res) => {
-            this.issues = res.data;
-        })
-      },
-      // 新しく作成したmethod
-      closeIssue(number){
-        client.patch(`/issues/${number}`, // --コード2
-            {
-              state: 'closed'
-            },
-          )
-          .then(() => {
-            this.issues.some((v, i) => { // --コード3
-              if(v.number == number) this.issues.splice(i, 1); // --コード4
-            })
-        })
-      },
+  <div>
+    <el-button type="success" round @click="getIssues()">Get Issue</el-button>
+    <el-row :gutter="12">
+      <TodoItem v-for="( issue, index ) in issues" :key="issue.id" :issue="issue.title" :index="index" @closeIssue="closeIssue"/>
+    </el-row>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import TodoItem from '@/components/TodoItem';
+
+const client = axios.create({
+  baseURL: `${process.env.VUE_APP_GITHUB_ENDPOINT}`,
+  headers: {
+    'Accept': 'application/vnd.github.v3+json',
+    'Content-Type':'application/json',
+    'Authorization': `token ${process.env.VUE_APP_GITHUB_TOKEN}`
+  },
+})
+
+export default {
+  name: 'IssueList',
+  components: {
+    TodoItem
+  },
+  data() {
+    return {
+      issues: [],
     }
+  },
+  methods: {
+    getIssues() {
+      client.get('/issues')
+        .then((res) => {
+          this.issues = res.data;
+      })
+    },
+    closeIssue(index){
+      const target = this.issues[index]
+      client.patch(`/issues/${target.number}`,
+          {
+            state: 'closed'
+          },
+        )
+        .then(() => {
+          this.issues.splice(index, 1)
+      })
+    },
   }
-  </script>
-  
+}
+</script>
--- src/views/TodoList.vue
+++ src/views/TodoList.vue
@@ -1,40 +1,69 @@
 <template>
-    <div>
-      <h1>todoリスト</h1>
-      <form @submit.prevent="addTodo()">
-        <input v-model="todo">
-      </form>
-      <el-row :gutter="12">
-        <el-col :span="12"  v-for="( todo, index ) in todos" :key="index">
-          <el-card class="box-card" shadow="hover" style="margin: 5px 0;">
-            <el-row :gutter="12">
-              <el-col :span="21">{{ todo }}</el-col>
-              <el-col :span="3">
-                <el-button @click="removeTodo(index)" type="success" icon="el-icon-check" circle></el-button>
-              </el-col>
-            </el-row>
-          </el-card>
-        </el-col>
-      </el-row>
-    </div>
-  </template>
-  <script>
-  export default {
-    name: 'TodoList',
-    data () {
-      return {
-        todo: '',
-        todos: []
-      }
-    },
-    methods: {
-      addTodo(){
-        this.todos.push(this.todo);
-        this.todo = '';
-      },
-      removeTodo(index){
-        this.todos.splice(index, 1);
-      }
+  <div>
+    <h1>TodoとIssueリスト</h1>
+    <form @submit.prevent="addTodo()">
+      <el-input placeholder="todo" v-model="todo"></el-input>
+    </form>
+    <el-row :gutter="12">
+      <TodoItem v-for="( todo, index ) in todos" :key="index" :todo="todo" @removeToDo="removeTodo"/>
+      <TodoItem v-for="( issue, index ) in issues" :key="issue.id" :issue="issue.title" :index="index" @closeIssue="closeIssue" />
+    </el-row>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import TodoItem from '@/components/TodoItem';
+
+const client = axios.create({
+  baseURL: `${process.env.VUE_APP_GITHUB_ENDPOINT}`,
+  headers: {
+    'Authorization': `token ${process.env.VUE_APP_GITHUB_TOKEN}`,
+    'Accept': 'application/vnd.github.v3+json',
+    'Content-Type':'application/json',
+  },
+})
+
+export default {
+  name: 'TodosIssues',
+  components: {
+    TodoItem
+  },
+  data () {
+    return {
+      todo: '',
+      todos: [],
+      issues: []
     }
+  },
+  methods: {
+    addTodo(){
+      this.todos.push(this.todo);
+      this.todo= '';
+    },
+    removeTodo(index){
+      this.todos.splice(index, 1);
+    },
+    closeIssue(index){
+      const target = this.issues[index];
+      client.patch(`/issues/${target.number}`,
+          {
+            state: 'closed'
+          },
+        )
+        .then(() => {
+         this.issues.splice(index, 1);
+      })
+    },
+    getIssues() {
+      client.get('issues')
+        .then((res) => {
+          this.issues = res.data
+      })
+    }
+  },
+  created() {
+    this.getIssues();
   }
-  </script>
+}
+</script>
